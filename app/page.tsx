dd42545a05a4ecf886fd662fbddc990ad6fd6a44
Charles Lim Jun Hao <charleslimjh@u.nus.edu>
2024-02-22 16:57:23 +0800

    new menu page with 3-life and time modes using radios

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,102 @@
 "use client";
 
-import CardListColumn from "@/components/oneColCards";
-
-const gameModes = [
-  {
-    name: "All Lines",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from all lines included.
-      Skipping a station costs you 10 seconds.`,
-    link: "/alllines",
-  },
-  {
-    name: "North South Line",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from the North South Line (Red Line).
-      Skipping a station costs you 10 seconds.`,
-    link: "/nsl",
-  },
-  {
-    name: "East West Line",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from the East West Line (Green Line).
-      Skipping a station costs you 10 seconds.`,
-    link: "/ewl",
-  },
-  {
-    name: "North East Line",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from the North East Line (Purple Line).
-      Skipping a station costs you 10 seconds.`,
-    link: "/nel",
-  },
-  {
-    name: "Circle Line",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from the North South Line (Yellow Line).
-      Skipping a station costs you 10 seconds.`,
-    link: "/ccl",
-  },
-  {
-    name: "Downtown Line",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from the Downtown Line (Blue Line).
-      Skipping a station costs you 10 seconds.`,
-    link: "/dtl",
-  },
-  {
-    name: "Thomson-East Coast Line",
-    description: `Identify as many stations from their abbreviations in 90 seconds, 
-      with stations from the Thomson-East Coast Line (Brown Line).
-      Skipping a station costs you 10 seconds.`,
-    link: "/tel",
-  },
-];
+import { Button } from "@nextui-org/button";
+import { RadioGroup, Radio } from "@nextui-org/radio";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { stringify } from "querystring";
 
 export default function Page() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      mode: "time",
+      line: "all",
+    },
+  });
+
   return (
+    <div>
+      <div className="pb-10">
+        <h1 className="text-5xl pb-10">MRT Abbreviation Game</h1>
+      </div>
+      <div className="text-left">
+        <form
+          onSubmit={handleSubmit((data) =>
+            router.push(`${data.line}_${data.mode}`)
+          )}
+        >
+          <RadioGroup
+            id="gameModeRadio"
+            label="Select your game mode:"
+            isRequired
+            defaultValue="time"
+            {...register("mode")}
+          >
+            <Radio
+              value="time"
+              description="Time attack, 90 second to guess as many stations as you can."
+              {...register("mode")}
+            >
+              Time Attack - 90 second
+            </Radio>
+            <Radio
+              value="lives"
+              description="Accuracy challenge, try to identify all stations in a line!"
+              {...register("mode")}
+            >
+              100% - 3 Lives
+            </Radio>
+          </RadioGroup>
+          <br />
+          <RadioGroup
+            id="gameLineRadio"
+            label="Select the MRT Line:"
+            defaultValue="all"
+            isRequired
+            {...register("line")}
+          >
+            <Radio id="all" value="all" color="default" {...register("line")}>
+              All Lines
+            </Radio>
+            <Radio id="nsl" value="nsl" color="danger" {...register("line")}>
+              North-South Line (NSL)
+            </Radio>
+            <Radio id="ewl" value="ewl" color="success" {...register("line")}>
+              East-West Line (EWL)
+            </Radio>
+            <Radio id="nel" value="nel" color="secondary" {...register("line")}>
+              North East Line (NEL)
+            </Radio>
+            <Radio id="ctl" value="ccl" color="warning" {...register("line")}>
+              Circle Line (CCL)
+            </Radio>
+            <Radio id="dtl" value="dtl" color="primary" {...register("line")}>
+              Downtown Line (DTL)
+            </Radio>
+            <Radio id="tel" value="tel" color="warning" {...register("line")}>
+              Thomson-East Coast Line (TEL)
+            </Radio>
+          </RadioGroup>
+
+          <br />
+          <Button type="submit">Play</Button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+/*
+return (
     <div className="pb-10">
       <h1 className="text-5xl pb-10">MRT Abbreviation Game</h1>
 
       <CardListColumn cardItems={gameModes} />
     </div>
   );
-}
+*/
