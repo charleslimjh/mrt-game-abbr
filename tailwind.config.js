294e52746d8431ea823db7853d51917760f2e690
Charles Lim Jun Hao <charleslimjh@u.nus.edu>
2024-02-29 15:29:40 +0800

    configure pulse animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "pulse-bg": "pulse-bg 0.3s linear forwards",
+      },
+      keyframes: {
+        "pulse-bg": {
+          "0%": { backgroundColor: "var(--tw-gradient-from)" },
+          "50%": { backgroundColor: "var(--tw-gradient-to)" },
+          "100%": { backgroundColor: "var(--tw-gradient-from)" },
+        },
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
