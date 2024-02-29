fc975ed5aed725ee9ef9de13314a4e153fee9802
Charles Lim Jun Hao <charleslimjh@u.nus.edu>
2024-02-29 14:29:11 +0800

    revert unicons due to defaultProps issue

--- components/statusLives.tsx
+++ components/statusLives.tsx
@@ -1,22 +1,9 @@
-import { UilHeart, UilHeartBreak } from "@iconscout/react-unicons";
-
 export default function StatusLives(props: any) {
   return (
     <div>
       {props.score < props.total && `Score: ${props.score} / ${props.total}`}
       <br />
-      <div className="flex justify-center">
-        {Array(props.livesLeft)
-          .fill(0)
-          .map((_, x) => (
-            <UilHeart color="#FF0000" key={x} />
-          ))}
-        {Array(3 - props.livesLeft)
-          .fill(0)
-          .map((_, x) => (
-            <UilHeartBreak color="#FF0000" key={x} />
-          ))}
-      </div>
+      <div className="flex justify-center">Lives: {props.livesLeft}</div>
     </div>
   );
 }
