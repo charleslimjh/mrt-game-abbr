abb7cd37c82075719be1e01bcb4ea3e6d62be732
Charles Lim Jun Hao <charleslimjh@u.nus.edu>
2024-02-29 09:57:30 +0800

    import unicons and heart icons for lives

--- components/statusLives.tsx
+++ components/statusLives.tsx
@@ -1,9 +1,22 @@
+import { UilHeart, UilHeartBreak } from "@iconscout/react-unicons";
+
 export default function StatusLives(props: any) {
   return (
     <div>
       {props.score < props.total && `Score: ${props.score} / ${props.total}`}
       <br />
-      Lives left: {props.livesLeft}
+      <div className="flex justify-center">
+        {Array(props.livesLeft)
+          .fill(0)
+          .map((_, x) => (
+            <UilHeart color="#FF0000" key={x} />
+          ))}
+        {Array(3 - props.livesLeft)
+          .fill(0)
+          .map((_, x) => (
+            <UilHeartBreak color="#FF0000" key={x} />
+          ))}
+      </div>
     </div>
   );
 }
